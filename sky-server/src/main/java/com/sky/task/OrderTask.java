31f9729276e077f01ec9d2afb97d0346b4ff4c09
lamyh <hoho1312465755@qq.com>
2026-04-27 16:19:10 +0800

    订单状态定时处理（包括了不会实现的微信支付功能）

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,65 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+/**
+ * 定时任务类，定时处理订单、状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单的方法, cron可通过网站生成表达式
+     */
+    @Scheduled(cron = "0 * * * * ?")  //每分钟触发一次
+    //@Scheduled(cron = "1/5 * * * * ?")
+    public void processTimeoutOrder() {
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes((-15));
+        //select * from orders where status = ? and order_time < (当前时间 - 15分钟)
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(ordersList.size() > 0 && ordersList != null){
+            for(Orders order : ordersList){
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("订单超时，自动取消");
+                order.setCancelTime(LocalDateTime.now());
+                orderMapper.update(order);
+            }
+        }
+    }
+
+    /**
+     * 处理一直处于派送中的订单
+     */
+    @Scheduled(cron = "0 0 1 * * ?") //每天凌晨1点出发一次
+    //@Scheduled(cron = "0/5 * * * * ?")
+    public void processDeliveryOrder() {
+        log.info("定时处理一直处于派送中的订单：{}", LocalDateTime.now());
+
+        //查询有没有这种订单
+        LocalDateTime time = LocalDateTime.now().plusMinutes((-60));
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+        if(ordersList.size() > 0 && ordersList != null){
+            for(Orders order : ordersList){
+                order.setStatus(Orders.COMPLETED);
+                orderMapper.update(order);
+            }
+        }
+
+    }
+}
